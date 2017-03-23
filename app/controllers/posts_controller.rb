2ea8d682e0ff3348e0511305f7d8e05cc4ddef59
Amjad Hussain <amjad.hussain544@gmail.com>
2017-03-24 03:24:29 +0500

    Added devise and users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
 	def index
 		@posts = Post.all.order('created_at DESC')
 	end
@@ -8,7 +9,7 @@ class PostsController < ApplicationController
 	end
 	
 	def create
-		@post = Post.new(post_params)
+		@post = Post.new(post_params) 
 		if @post.save
 			redirect_to @post
 		else
